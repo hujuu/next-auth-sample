85d55a81be5abc5b55ddf98cb251bee70333e3d0
hujuu <1961946+hujuu@users.noreply.github.com>
2023-01-20 21:45:21 +0900

    Update [...nextauth].ts

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -4,6 +4,7 @@ import GithubProvider from "next-auth/providers/github"
 import TwitterProvider from "next-auth/providers/twitter"
 import Auth0Provider from "next-auth/providers/auth0"
 import DribbbleProvider from "../../src/dribbble";
+import QiitaProvider from "../../src/qiita";
 
 // For more information on each option (and a full list of options) go to
 // https://next-auth.js.org/configuration/options
@@ -21,6 +22,7 @@ export const authOptions: NextAuthOptions = {
     TwitterProvider({
       clientId: process.env.TWITTER_ID,
       clientSecret: process.env.TWITTER_SECRET,
+      version: "2.0",
     }),
     Auth0Provider({
       clientId: process.env.AUTH0_ID,
@@ -28,8 +30,12 @@ export const authOptions: NextAuthOptions = {
       issuer: process.env.AUTH0_ISSUER,
     }),
     DribbbleProvider({
-      clientId: process.env.DRIBBBLE_ID,
-      clientSecret: process.env.DRIBBBLE_SECRET,
+      clientId: process.env.DRIBBBLE_ID!,
+      clientSecret: process.env.DRIBBBLE_SECRET!,
+    }),
+    QiitaProvider({
+      clientId: process.env.QIITA_ID,
+      clientSecret: process.env.QIITA_SECRET,
     }),
   ],
   theme: {

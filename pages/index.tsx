5bbccfcd6a98b0cd3f50e8673d12127ff56406fe
hujuu <1961946+hujuu@users.noreply.github.com>
2023-01-20 22:18:06 +0900

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,10 +4,7 @@ export default function IndexPage() {
   return (
     <Layout>
       <h1>NextAuth.js Example</h1>
-      <p>
-        This is an example site to demonstrate how to use{" "}
-        <a href="https://next-auth.js.org">NextAuth.js</a> for authentication.
-      </p>
+      <p>これは認証のための<a href="https://next-auth.js.org">NextAuth.js</a>の使い方をデモンストレーションする実例のサイトです。</p>
     </Layout>
   )
 }

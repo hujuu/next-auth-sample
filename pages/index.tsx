f8d36e7331710066527b94ea79308fcd7dd7aa96
hujuu <1961946+hujuu@users.noreply.github.com>
2023-01-20 22:21:16 +0900

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,8 +3,8 @@ import Layout from "../components/layout"
 export default function IndexPage() {
   return (
     <Layout>
-      <h1>NextAuth.js Example</h1>
-      <p>これは認証のための<a href="https://next-auth.js.org">NextAuth.js</a>の使い方をデモンストレーションする実例のサイトです。</p>
+      <h1>NextAuth.js サンプル</h1>
+      <p>これは<a href="https://next-auth.js.org">NextAuth.js</a>の使い方をデモンストレーションするサイトです。</p>
     </Layout>
   )
 }
